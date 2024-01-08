4f54e019fde4aafcb9ca26763518eafe3ca5d52f
Artur <artur.piasecki158@gmail.comgit config --global user.email artur.piasecki158@gmail.comgit config --list$ git config --global user.email artur.piasecki158@gmail.com>
2024-01-08 21:48:52 +0000

    newChanges

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Password-Generator
 Website with JavaScript that allows to generate random passwords for the users
+
+This JavaScript application generates a random password based on user preferences for length and character types
+
+Features
+
+-Customizable Password Length: Allows the user to specify the desired length of the password (minimum length: 8 characters).
+-Character Type Selection: Provides options for including lowercase letters, numeric characters, and special characters in the generated password.
+-Generates a random password based on user choices upon clicking the "Generate Password" button.
+
+How to Use
+Clone the repository or download the source code.
+
+Open index.html in a modern web browser.
+
+Click on the "Generate Password" button to start the process.
+
+Follow the prompts to select the password length and character types to include.
+
+Once all selections are made, the generated password will be displayed in the designated area.
+
+Usage Notes
+
+-Make sure the password length is at least 8 characters
+-Selecting multiple character types 
+-The generated password can be copied
+
+Technology Used
+
+-HTML
+-CSS
+-JavaScript
+
+
+This project is licensed under the MIT License. See the LICENSE file for details.
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Array of special characters to be included in password
+//* Array of special characters to be included in password
 var specialCharacters = [
   '@',
   '%',
@@ -87,16 +87,54 @@ var upperCasedCharacters = [
   'Y',
   'Z'
 ];
-function getPasswordOptions() {
+function getPasswordLength() {
   while (true) {
     var lengthOfPassword = prompt("Please enter how many characters you'd like:");
 
-    if (lengthOfPassword < 8) {
+
+    var numericLength = +lengthOfPassword;
+
+    if (numericLength < 8) {
       alert("The password must have a minimum of 8 characters. Please try again.");
     } else {
-      return parseInt(lengthOfPassword);
+      return numericLength;
     }
   }
+}
+
+
+function getUserChoice(message) {
+  return confirm(message);
+}
+
+function generatePassword() {
+  var password = '';
+
+  var passwordLength = getPasswordLength();
+  var includeLowercase = getUserChoice("Would you like to include lowercase letters?");
+  var includeNumeric = getUserChoice("Would you like to include numeric characters?");
+  var includeSpecialChars = getUserChoice("Would you like to include special characters?");
+
+  var availableChars = upperCasedCharacters;
+
+  if (includeLowercase) {
+    availableChars = availableChars.concat(lowerCasedCharacters);
+  }
+
+  if (includeNumeric) {
+    availableChars = availableChars.concat(numericCharacters);
+  }
+
+  if (includeSpecialChars) {
+    availableChars = availableChars.concat(specialCharacters);
+  }
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * availableChars.length);
+    password += availableChars[randomIndex];
+  }
+
+  return password;
 }
     //var lowercaseOption = "Would you like your password to include lowercase letters?";
   //  if (confirm(lowercaseOption)== true ) {
@@ -111,25 +149,25 @@ function getPasswordOptions() {
 //NEXT BOX ABOUT SPECIAL CHARACTERS SAME PRINCIPAL
 //WANT THE PROGRAM TO RUN THE CHOISES OF THE USERS INPUT AND GENERATE RANDOM PASSWORD ON THAT INPUT
 
-var lowercaseOption = prompt("Would you like your password to include lowercase letters?");
-  if (confirm(lowercaseOption)) {
-    console.log("User wants lowercase letters in the password");
+//var lowercaseOption = prompt("Would you like your password to include lowercase letters?");
+ // if (confirm(lowercaseOption)) {
+   // console.log("User wants lowercase letters in the password");
     // Include lowercase characters in the password
-    return lowerCasedCharacters;
-  } else {
-    console.log("User does not want lowercase letters in the password");
+   // return lowerCasedCharacters;
+ // } else {
+  //  console.log("User does not want lowercase letters in the password");
     // Exclude lowercase characters in the password
-    return upperCasedCharacters;
-  }
-}
+  //  return upperCasedCharacters;
+ // }
+//}
 
 // Example usage:
-var passwordLength = getPasswordOptions();
-if (passwordLength) {
-  console.log("The user wants a password of length: " + passwordLength);
+//var passwordLength = getPasswordOptions();
+//if (passwordLength) {
+  //console.log("The user wants a password of length: " + passwordLength);
   
   // Use passwordLength in further processing if it meets the criteria
-}
+//}
 // Function to prompt user for password options
 //function getPasswordOptions() {
  //var lenghtOfPassword = prompt("Please enter what how many characters you'd like",);
@@ -139,28 +177,28 @@ if (passwordLength) {
 
 
 // Function for getting a random element from an array
-function getRandom(arr) {
+//function getRandom(arr) {
 
-console.log (g)
-}
+//console.log (g)
+//}
 
-var allCharacters = upperCasedCharacters + lowerCasedCharacters + numericCharacters +specialCharacters;
+//var allCharacters = upperCasedCharacters + lowerCasedCharacters + numericCharacters +specialCharacters;
 // Function to generate password with user input
-function generatePassword() {
+//function generatePassword() {
 
-  console.log("you clicked a button!") //this works
+//  console.log("you clicked a button!") //this works
 
-  var password = "";
+//  var password = "";
 
-  password += upperCasedCharacters[Math.floor(Math.random() * upperCasedCharacters.length)];
-  password += lowerCasedCharacters[Math.floor(Math.random() * lowerCasedCharacters.length)];
-  password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-  password += numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
+//  password += upperCasedCharacters[Math.floor(Math.random() * upperCasedCharacters.length)];
+  ///password += lowerCasedCharacters[Math.floor(Math.random() * lowerCasedCharacters.length)];
+ // password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+ // password += numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
 
 
   
-  return  password
-}
+ // return  password
+//}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
